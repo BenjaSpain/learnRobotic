8c89b40ea7cabf3fbe12417d0e3fe9d3e692dead
BenjaSpain <55950461+BenjaSpain@users.noreply.github.com>
2026-05-14 12:46:10 +0200

    [FEAT]: Add environment config file

---
+++ ros2_env_conf.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# ros2_env_conf.sh
+#    - To current Terminal keep sourced and apply changes.
+#    - RUN as: source ros2_env_conf.sh
+
+# Source ROS2 environment into current terminal
+source /opt/ros/jazzy/setup.bash
+
+#Define ROS_DOMAIN_ID = 10
+export ROS_DOMAIN_ID=10
+#echo "export ROS_DOMAIN_ID=10" >> ~/.bashrc
